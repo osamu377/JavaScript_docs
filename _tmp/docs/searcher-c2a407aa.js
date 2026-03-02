bb94bc9524616fcade1f295d6d29177d22679cc2
Osamu377 <osamu-ochi@outlook.com>
2026-03-02 16:49:38 +0900

    docsディレクトリの場所を変更

--- _tmp/docs/searcher-c2a407aa.js
+++ _tmp/docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-78abab6b.js',
+                path_to_root + 'searchindex-ebef5985.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
