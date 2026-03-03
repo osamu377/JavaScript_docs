d89738875b6aa37b5306b97a01a0f97d6d4bd180
Osamu377 <osamu-ochi@outlook.com>
2026-03-04 00:21:25 +0900

    付録のブラッシュアップ

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e0eeb4bb.js',
+                path_to_root + 'searchindex-ed5d5011.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
