b3c5e15c194836522bdb1f2f5989b4c5f10926ad
Osamu377 <osamu-ochi@outlook.com>
2026-03-05 14:00:40 +0900

    青空文庫形式のルビ

--- your_1st_website/book/html/searcher-c2a407aa.js
+++ your_1st_website/book/html/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b457424d.js',
+                path_to_root + 'searchindex-56c46f31.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
