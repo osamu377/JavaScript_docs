e07964e596d476c14ed900ea534ea33d6ba74f69
Osamu377 <osamu-ochi@outlook.com>
2026-03-05 16:33:53 +0900

    一通り仕上げた

--- your_1st_website/book/html/searcher-c2a407aa.js
+++ your_1st_website/book/html/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-56c46f31.js',
+                path_to_root + 'searchindex-37e3fdbd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
